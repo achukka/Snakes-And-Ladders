64f460608ae66834b4f6efccf62f7f5e18faf078
unknown <achukka@.corp.ebay.com>
2014-07-07 16:31:45 +0530

    LAZY initial project commit

---
+++ GameBoard.java
@@ -0,0 +1,16 @@
+
+public class GameBoard {
+	public GameBoard() {
+	
+	}
+	
+	public BoardSquare getStartSquare()
+	{
+		return null;
+	}
+	
+	public PlayerPiece movePlayerPiece(PlayerPiece player)
+	{
+		return null;
+	}
+}
